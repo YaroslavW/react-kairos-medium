25e5db28092cd830442a1b09304758651cef4d80
Yaroslav <doctor355@yahoo.com>
2019-06-29 06:10:27 +0300

    Creation of site structure and division into components.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Header from './Components/Header';
+import Home from './Components/Home';
+import About from './Components/About';
+import AboutHow from './Components/AboutHow';
+import Features from './Components/Features';
+import Pricing from './Components/Pricing';
+import Download from './Components/Download';
+import Footer from './Components/Footer';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header />
+      <Home />
+      <About />
+      <AboutHow />
+      <Features />
+      <Pricing />
+      <Download />
+      <Footer />
     </div>
   );
 }
---
+++ src/Components/AboutHow.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AboutHow = () => {
+  return (
+    <div>
+      About-How
+    </div>
+  );
+};
+
+export default AboutHow;
